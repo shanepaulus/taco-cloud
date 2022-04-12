a9a33937782357095a07f3ddf13f8ef76ee6c39b
Shane Paulus <shanepaulus@gmail.com>
2022-04-12 23:27:22 +0200

    Added Bootstrap

--- src/main/java/tacos/Application.java
+++ src/main/java/tacos/Application.java
@@ -1,10 +1,18 @@
 package tacos;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Shane Paulus
  * <p>
  * Date Created : 12-Apr-2022.
  */
 
+@SpringBootApplication
 public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
