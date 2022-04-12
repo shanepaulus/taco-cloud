034cd5ed3351692d4d51474f9ff9735d5693f6d3
Shane Paulus <shanepaulus@gmail.com>
2022-04-12 23:38:30 +0200

    Added Boot Context test class

---
+++ src/test/java/tacos/ApplicationTest.java
@@ -0,0 +1,22 @@
+package tacos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Shane Paulus
+ * <p>
+ * Date Created : 12-Apr-2022.
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+  @Test
+  public void contextLoads() {
+    
+  }
+}
